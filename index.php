c32941d860f84333d41a8054dbdf05407f71bca3
Nadadalila <fidhinafinadalilaaa@gmail.com>
2023-05-10 21:07:44 +0700

    buat button create

--- index.php
+++ index.php
@@ -7,17 +7,34 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <title>Document</title>
 </head>
+<style>
+    .header, h3{
+        text-align: center;
+        color: black;
+        
+    }
+    .header {
+        background-color : yellow;
+        height : 40px;
+        margin-bottom: 20px;
+    }
+</style>
 
 <body>
     <div class="container">
-        <h3 style="text-align:center" class="mt-5">DATA PELANGGAN</h3> <br> <br>
+        <div class="header">
+            <h3 style="text-align:center" class="mt-5">DATA PELANGGAN TOKO KUE</h3> <br> <br>
+        </div>
+
+        <a href="create.php" class="btn btn-primary lg mb-4" >Create</a>
+
         <table cellpadding ="10" cellspacing="0" class= "table table-striped">
             <tr>
                 <th width="50px" class="text-center">id_pembeli</th>
                 <th width="200px" class="text-center">nama_pembeli</th>
                 <th width="200px" class="text-center">alamat</th>
                 <th width="200px" class="text-center">jenis_kelamin</th>
-                <th width="200px" class="text-center">nomor_hp</th>
+                <th width="200px" class="text-center">no_telepon</th>
                 <th width="200px" class="text-center">aksi</th>
             </tr>
 
